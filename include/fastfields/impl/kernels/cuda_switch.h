2ca71022a400ce0bc186bc710e24d8b296b7c311
balbasty <yael.balbastre@gmail.com>
2025-07-18 10:18:55 +0100

    Define aliases for __host__ __device__

--- include/fastfields/impl/kernels/cuda_switch.h
+++ include/fastfields/impl/kernels/cuda_switch.h
@@ -9,8 +9,16 @@
 #ifndef __host__
 #define __host__
 #endif
+#define CUGLOB
+#define CUHOST
+#define CUDEV
+#define CUHOSTDEV
 #else
 #include <cuda_fp16.h>
+#define CUGLOB __global__
+#define CUHOST __host__
+#define CUDEV __device__
+#define CUHOSTDEV __host__ __device__
 #endif
 
 #endif // FF_CUDA_SWITCH
