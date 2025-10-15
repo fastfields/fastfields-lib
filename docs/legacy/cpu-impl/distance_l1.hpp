a10e6b93823959df395b80e44c0e0e2c6f3a48b7
balbasty <yael.balbastre@gmail.com>
2025-10-15 12:13:02 +0100

    Add dispatcher struct so that input pointer can be void*

--- docs/legacy/cpu-impl/distance_l1.hpp
+++ docs/legacy/cpu-impl/distance_l1.hpp
@@ -10,26 +10,42 @@ FF_NAMESPACE_BEGIN(FF_DEVICE)
 FF_NAMESPACE_BEGIN(distance_l1)
 
 template <typename scalar_t, typename offset_t>
-void distance(
-          offset_t ndim,            // number of dimensions
-          scalar_t f        [],     // pointer to data [*batch, n]
-          scalar_t w,               // pixel spacing
-    const offset_t size     [],     // [ndim] data shape   == (*batch, n)
-    const offset_t stride   []      // [ndim] data strides
-)
-{
-    offset_t nbatch = ndim - 1;
-    offset_t n = size[nbatch];
-    offset_t s = stride[nbatch];
+struct Dispatch {
 
-    offset_t numel = prod(size, nbatch);
-    parallel_for(0, numel, GRAIN_SIZE, [&](long start, long end) {
-    for (offset_t i=start; i < end; ++i)
+    static inline void
+    dt(
+              offset_t ndim,            // number of dimensions
+              void     f        [],     // pointer to data [*batch, n]
+              scalar_t w,               // pixel spacing
+        const offset_t size     [],     // [ndim] data shape   == (*batch, n)
+        const offset_t stride   []      // [ndim] data strides
+    )
     {
-        offset_t offset = index2offset(i, nbatch, size, stride);
-        kernel(f + offset, n, s, w);
-    }});
-}
+        return dt(ndim, static_cast<scalar_t*>(f), w, size, stride);
+    }
+
+    static void
+    dt(
+              offset_t ndim,            // number of dimensions
+              scalar_t f        [],     // pointer to data [*batch, n]
+              scalar_t w,               // pixel spacing
+        const offset_t size     [],     // [ndim] data shape   == (*batch, n)
+        const offset_t stride   []      // [ndim] data strides
+    )
+    {
+        offset_t nbatch = ndim - 1;
+        offset_t n = size[nbatch];
+        offset_t s = stride[nbatch];
+
+        offset_t numel = prod(size, nbatch);
+        parallel_for(0, numel, GRAIN_SIZE, [&](long start, long end) {
+        for (offset_t i=start; i < end; ++i)
+        {
+            offset_t offset = index2offset(i, nbatch, size, stride);
+            kernel(f + offset, n, s, w);
+        }});
+    }
+};
 
 FF_NAMESPACE_END(distance_l1)
 FF_NAMESPACE_END(FF_DEVICE)
